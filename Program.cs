45946d661c5542837c184996fa3c2a762c40640b
jgrissom <jgrissom91@gmail.com>
2019-09-04 10:30:47 -0500

    Composite Formatting

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DateComposite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            Console.WriteLine("1. {0:MMMM} {0:dd}, {0:yyyy}", today);
+            Console.WriteLine("2. {0:yyyy}.{0:MM}.{0:dd}", today);
+            Console.WriteLine("3. Day {0:dd} of {0:MMMM}, {0:yyyy}", today);
+            Console.WriteLine("4. Year:{0:yyyy},Month:{0:MM},Day:{0:dd}", today);
+            Console.WriteLine("5. {0:dddd}", today);
+            Console.WriteLine("6. {0:hh}:{0:mm} {0:tt}", today);
+            Console.WriteLine("7. h:{0:hh},m:{0:mm},s:{0:ss}", today);
+            Console.WriteLine("8. {0:yyyy}.{0:MM}.{0:dd}.{0:hh}.{0:mm}.{0:ss}", today);
+        }
+    }
+}
